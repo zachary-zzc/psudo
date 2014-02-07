f8f6c8dd9131bc19134204ff77ab6ae148adda40
zachary-zzc <shinaider.zhao@gmail.com>
2014-02-07 21:56:25 +0800

    first structure update

---
+++ structure/LinkList.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, value, p=0, *args, **kwargs):
+        self.data = value
+        self.next = p
+
+class LinkList:
+    def __init__(self):
+        pass
+
--- structure/array.py
+++ structure/array.py
@@ -1,9 +1,34 @@
 class array:
-    def __init__(self):
+
+    def __init__(self, array=None, *args, **kwargs):
+        self.__array = array or []
+
+    def get(self):
+        return self.__array
+
+    def __str__(self):
         pass
 
-    def length(self):
+    def __repr__(self):
         pass
+
+    def __iter__(self):
+        pass
+
+    def next(self, length):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def __getattr__(self, attr):
+        pass
+
+    def __cmp__(self, obj):
+        pass
+
+    def __len__(self):
+        return len(self.array)
 
     def search(self):
         pass
