082d44f42e8a275f2a073b821c927f479b949f6c
zachary <shinaider.zhao@gmail.com>
2014-03-26 22:14:03 +0800

    fix tree init, str bug

--- structure/tree.py
+++ structure/tree.py
@@ -5,13 +5,16 @@ class Node:
     __class__ = 'Node'
 
     def __init__(self, value=None, children=[]):
-        self.value = value
-        self.children = children
+        self._val = value
+        self._children = children
 
     def __str__(self, level=0):
-        ret = '\t' * level + str(self.value) + '\n'
-        for child in self.children:
-            ret += child.__str__(level+1)
+        ret = '\t' * level + str(self._val) + '\n'
+        for child in self._children:
+            if isinstance(child, Node):
+                ret += child.__str__(level+1)
+            else:
+                ret += '\t' * (level+1) + str(child) + '\n'
         return ret
 
     __repr__ = __str__
@@ -23,55 +26,58 @@ class Node:
             yield child
 
     def __del__(self):
-        self.value = None
-        self.children = []
+        self._val = None
+        self._children = []
 
     def __eq__(self, obj):
-        return ((self.value == obj.value) and (self.children == obj.children))
+        return ((self._val == obj.value) and (self._children == obj.children))
 
     def __len__(self):
-        return len(self.children)
+        return len(self._children)
 
     def __getitem__(self, ind):
-        return self.children[ind]
+        return self._children[ind]
 
     def __setitem__(self, ind, val):
-        self.children[ind] = val
+        self._children[ind] = val
 
     def __delitem__(self, ind):
-        return self.children.pop(ind)
+        return self._children.pop(ind)
 
     def __getslice__(self, ind1, ind2):
-        return self.children[ind1:ind2]
+        return self._children[ind1:ind2]
 
     def __setslice__(self, ind1, ind2, val):
-        self.children[ind1:ind2] = val
+        self._children[ind1:ind2] = val
 
     def __delslice__(self, ind1, ind2):
         for ind in range(ind, ind2):
-            self.children.pop(ind)
+            self._children.pop(ind)
 
     @property
     def value(self):
-        return self.value
+        return self._val
 
     @property
     def children(self):
-        return self.children
+        return self._children
 
     @value.setter
     def value(self, value):
-        self.value = value
+        self._val = value
 
     @children.setter
     def children(self, children):
-        if (isinstance(value, list)):
-            self.children = children
+        if (isinstance(children, list)):
+            self._children = children
         else:
             raise Exception('Children should be list type')
 
+    def append(self, child):
+        self._children.append(child)
+
 class Tree(Node):
-    def __iter__(self, tree=None, value=None, children=[]):
+    def __init__(self, tree=None, value=None, children=[]):
         if tree is not None:
             if (isinstance(tree, Tree)):
                 self = tree
